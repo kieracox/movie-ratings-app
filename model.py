e0bc023e4d2d97186ad551e809e6d3c143ec33a9
Kiera Cox <kiera.cox18@gmail.com>
2023-04-11 16:52:36 -0700

    gitignore

--- model.py
+++ model.py
@@ -5,7 +5,46 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Replace this with your code!
+class User(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="user")
+
+    def __repr__(self):
+        return f"<User user_id={self.user_id} email={self.email}>"
+    
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="movie")
+
+    def __repr__(self):
+        return f"<Movie movie_id={self.movie_id} title={self.title}>"
+
+class Rating(db.Model):
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
+
+    def __repr__(self):
+        return f"<Rating rating_id={self.rating_id} score={self.score}>"
+
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
